3b4c0b94c8d6db8811823beab7b0937948f29c9e
macbook <macbook@MacBook-Air-de-Eduardo.local>
2025-03-28 01:00:27 -0300

    Exercício criado

---
+++ script.js
@@ -0,0 +1,18 @@
+function tabuada() {
+    let num = window.document.getElementById('txtnum')
+    let tab = window.document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('[ERRO]! Este não é um número válido!')
+    }
+
+    let number = Number(num.value)
+
+    tab.innerHTML = ''
+
+    for (let contador = 1; contador <= 10; contador++) {
+        let item = document.createElement('option')
+        item.text += `${number} x ${contador} = ${contador * number}`
+        tab.appendChild(item)
+    }
+}   
